be5ec0c387803d8fed9aba22ce378521dabe446e
Abdallah <abdallah.moubarak02@gmail.com>
2022-12-31 23:59:56 +0200

    adding details to product

--- app/components/ProductDetails.tsx
+++ app/components/ProductDetails.tsx
@@ -21,6 +21,29 @@ export default function ProductDetails() {
       <div className="flex justify-center align-center border-gray-200 border-t border-b p-4">
         <Image src="/mobile.png" alt="" width={300} height={300} />
       </div>
+      <div className="flex justify-between items-center p-1">
+        <div className="flex gap-2 items-center">
+          <div className="border border-gray-200 rounded-full w-max p-1">
+            <Image src="/svg/logo.svg" alt="" width={20} height={20} />
+          </div>
+          <div>546.89 X</div>
+        </div>
+        <div className="border border-gray-200 rounded-xl px-2 py-0 cursor-pointer">
+          Buy
+        </div>
+      </div>
+      <div className="text-left px-4 py-2">{text}</div>
     </div>
   );
 }
+
+const text: string = `50% Cotton, 50% Polyester
+Imported
+Machine Wash
+A HEAVYWEIGHT CHAMPION - Hanes sweatshirts for men are made from soft, plush, 9.7-oz. heavyweight fleece for cozy warmth.
+THE FIT YOU LOVE – These men’s workout sweatshirt boast a standard fit with dropped shoulders for a little extra room.
+OVER EASY - Classic men's crew sweatshirt never goes out of style.
+QUALITY FABRIC - The heavyweight cotton-rich fleece is made with Hanes' 
+A HEAVYWEIGHT CHAMPION - Hanes sweatshirts for men are made from soft, plush, 9.7-oz. heavyweight fleece for cozy warmth.
+THE FIT YOU LOVE – These men’s workout sweatshirt boast a standard fit with dropped shoulders for a little extra room.
+`;
